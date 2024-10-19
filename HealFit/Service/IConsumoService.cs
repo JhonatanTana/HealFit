9786a30ac87a6adfa20c51c5fa20e2f7595cd286
Carlota <gabriel.carlota@alunos.unis.edu.br>
2024-10-19 18:33:16 -0300

    .

---
+++ HealFit/Service/IConsumoService.cs
@@ -0,0 +1,11 @@
+﻿using HealFit.Model;
+
+namespace HealFit.Service;
+public interface IConsumoService {
+    Task<List<ProdutoConsumido>> GetConsumoByUserId(int id);
+    Task<bool> AddConsumo(ProdutoConsumido consumo);
+    Task<bool> UpdateConsumo(ProdutoConsumido consumo);
+    Task<bool> DeleteConsumo(int id);
+    Task<List<ProdutoConsumido>> GetAllConsumoByDate(int id, DateTime data);
+    Task<ProdutoConsumido> GetConsumoById(int id);
+}
